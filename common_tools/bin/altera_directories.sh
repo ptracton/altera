04e0636d11b3aaa461cf28f25cf72c66f30a402f
ptracton@gmail.com <ptracton@gmail.com>
2014-06-06 07:46:23 -0700

    Create directories for projects

---
+++ common_tools/bin/altera_directories.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p  rtl
+mkdir -p  simulation
+mkdir -p  synthesis
+mkdir -p  testbench
+mkdir -p  behavioral
+mkdir -p  qsys
+mkdir -p  software/bsp
+mkdir -p  software/applications
